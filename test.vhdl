580f1574d0e96038cdec8d01fa43f87f163821a4
infirit89 <georgedautov808@gmail.com>
2026-01-08 23:10:51 +0200

    basic signals and wait on/until

--- test.vhdl
+++ test.vhdl
@@ -2,16 +2,26 @@ entity test is
 end entity;
 
 architecture sim of test is
+	signal test1 : integer := 0;
+	signal test2 : integer := 10;
 begin
 	process is
-		variable toma : integer := 10;
 	begin
-		while toma < 20 loop
-			report "aaaaaaa";
-			report integer'image(toma);
-			wait for 1000 sec;
-			toma := toma + 2;
-		end loop;
+		test1 <= test1 + 1;
+		wait for 10 ns;
+	end process;
+
+	process is
+	begin
+		wait until test1 = test2;
+		report "They equal" & integer'image(test1);
+		wait;
+	end process;
+
+	process is
+	begin
+		wait on test1;
+		report "It changed" & integer'image(test1);
 		wait;
 	end process;
 end architecture;
