7a4536c9d37eb59cdc644629eebbfab58074be5f
infirit89 <georgedautov808@gmail.com>
2026-01-08 23:25:19 +0200

    process sensitivity list

--- test.vhdl
+++ test.vhdl
@@ -3,24 +3,30 @@ end entity;
 
 architecture sim of test is
 	signal test1 : integer := 0;
+	signal test2 : integer := 0;
 begin
 	process is
 	begin
 		test1 <= test1 + 1;
+		test2 <= test2 - 1;
 		wait for 10 ns;
 	end process;
 
 	process is
 	begin
-		if test1 = 14 then
-			report "It do be 14";
-		elsif test1 > 14 then
-			wait;
-		else
-			report "It don't be bigger than 14";
+		if test1 = test2 then
+			report "They do be equal";
 		end if;
 
-		wait on test1;
+		wait on test1, test2;
 	end process;
+
+	process(test1, test2) is
+	begin
+		if test1 = test2 then
+			report "They do be equal 2 electric boogaloo";
+		end if;
+	end process;
+
 
 end architecture;
